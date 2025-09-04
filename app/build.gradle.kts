2f1b9a3c4bc693f5274edcf5e72e68e7b2cfb0f7
ofygel <ofygel@gmail.com>
2025-09-04 08:08:17 +0500

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     // Корутины
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
