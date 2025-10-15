c467a19ec93994a64454f5014a433bdf914924b4
ofygel <ofygel@gmail.com>
2025-10-16 02:49:36 +0500

    Add regression test for MainViewModel instantiation

---
+++ app/src/test/java/com/example/abys/logic/MainViewModelInstantiationTest.kt
@@ -0,0 +1,14 @@
+package com.example.abys.logic
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import org.junit.Test
+
+class MainViewModelInstantiationTest {
+    @Test
+    fun `new instance factory can construct main viewmodel`() {
+        val provider = ViewModelProvider(ViewModelStore(), ViewModelProvider.NewInstanceFactory())
+
+        provider[MainViewModel::class.java]
+    }
+}
