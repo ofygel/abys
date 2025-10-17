1f221a6b4b8153d90fbe2fa13f8e299c2638d72b
ofygel <ofygel@gmail.com>
2025-10-18 00:17:22 +0500

    Import widthIn and heightIn modifiers

--- app/src/main/java/com/example/abys/ui/screen/CitySheet.kt
+++ app/src/main/java/com/example/abys/ui/screen/CitySheet.kt
@@ -25,10 +25,12 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
